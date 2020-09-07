3b37910d8bcee353210dbd1eb3d61a7e0d913fb8
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-09-08 00:59:44 +0400

    create class BinaryTree, StudentTest

---
+++ EPAM.2020.Latuhin.10/BinarySearchTree.CUI/BinarySearchTree.Core/BinaryTreeNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree.Core
+{
+    class BinaryTreeNode
+    {
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+        public IComparable Value { get; set; }
+
+    }
+}
---
+++ EPAM.2020.Latuhin.10/BinarySearchTree.CUI/BinarySearchTree.Core/TestStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree.Core
+{
+    class TestStudent
+    {
+        public string NameStudent { get; set; }
+        public string NameTest { get; set; }
+        public DateTime DatePassing { get; set; }
+        public int Score { get; set; }
+    }
+}
