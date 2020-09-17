b346a147e425432e5971853b43bf21cfb3ca59f1
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-09-17 22:38:12 +0400

    Add implementation of methods for opening, viewing, editing, saving a file

--- EPAM.2020.Latuhin.12/TextEditor.GUI/TextEditor.GUI/MainWindow.xaml.cs
+++ EPAM.2020.Latuhin.12/TextEditor.GUI/TextEditor.GUI/MainWindow.xaml.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace TextEditor.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private string _pathToFile;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,12 +20,91 @@ namespace TextEditor.GUI
 
         private void Load_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var ofd = new OpenFileDialog {Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*"};
+
+            if (ofd.ShowDialog() != true) return;
+
+            var document = new TextRange(DocBox.Document.ContentStart, DocBox.Document.ContentEnd);
+
+            try
+            {
+                _pathToFile = ofd.FileName;
+
+                using (var fs = new FileStream(_pathToFile, FileMode.Open))
+                {
+                    switch (Path.GetExtension(ofd.FileName).ToLower())
+                    {
+                        case ".txt":
+                            document.Load(fs, DataFormats.Text);
+                            break;
+                        case ".rtf":
+                            document.Load(fs, DataFormats.Rtf);
+                            break;
+                        default:
+                            document.Load(fs, DataFormats.Xaml);
+                            break;
+                    }
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine(@"Ошибка открытия файла");
+            }
+
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var document = new TextRange(DocBox.Document.ContentStart, DocBox.Document.ContentEnd);
+
+            using (var fs = File.Create(_pathToFile))
+            {
+                switch (Path.GetExtension(_pathToFile).ToLower())
+                {
+                    case ".txt":
+                        document.Save(fs, DataFormats.Text);
+                        break;
+                    case ".rtf":
+                        document.Save(fs, DataFormats.Rtf);
+                        break;
+                    default:
+                        document.Save(fs, DataFormats.Xaml);
+                        break;
+                }
+
+                MessageBox.Show("Изменения сохранены");
+            }
+        }
+
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog
+            {
+                Filter =
+                    "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*"
+            };
+
+            if (sfd.ShowDialog() != true) return;
+
+            var document = new TextRange(DocBox.Document.ContentStart, DocBox.Document.ContentEnd);
+            using (var fs = File.Create(sfd.FileName))
+            {
+                switch (Path.GetExtension(sfd.FileName).ToLower())
+                {
+                    case ".txt":
+                        document.Save(fs, DataFormats.Text);
+                        break;
+                    case ".rtf":
+                        document.Save(fs, DataFormats.Rtf);
+                        break;
+                    default:
+                        document.Save(fs, DataFormats.Xaml);
+                        break;
+                }
+
+                MessageBox.Show("Файл сохранен");
+            }
         }
     }
 }
