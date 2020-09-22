bf19419332a6fe54082082f1958d84e4fc175a4f
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-09-22 22:56:02 +0400

    add Entities, add test method for select data with filter category

---
+++ EPAM.2020.Latuhin.14/North/NorthEntityFramework.Tests/CodeFirstTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthEntityFramework.CodeFirst;
+
+namespace NorthEntityFramework.Tests
+{
+    [TestClass]
+    public class CodeFirstTest
+    {
+		[TestMethod]
+        public void ShouldReturn_OrderDetails_WhenInputWhereParameter()
+        {
+            using (var db = new NorthwindContext())
+            {
+                // categoryId = 1 (Beverages)
+                // categoryId = 2 (Condiments)
+                // categoryId = 3 (Confections)
+                var result = from order in db.Orders
+                    join orderDetail in db.OrderDetails on order.OrderId equals orderDetail.OrderId
+                    join product in db.Products on orderDetail.ProductId equals product.ProductId
+                    join category in db.Categories on product.CategoryId equals category.CategoryId
+                    join customer in db.Customers on order.CustomerId equals customer.CustomerId
+                    where category.CategoryId == 2
+                    select new
+                    {
+                        customerCompanyName = customer.CompanyName,
+                        customerContractName = customer.ContactName,
+                        Category = category.CategoryName,
+                        CategoryDescription = category.Description,
+                        productName = product.ProductName
+                    };
+
+                foreach (var res in result)
+                {
+                    Console.WriteLine($"{res.customerCompanyName} {res.customerContractName} " +
+                                      $"{res.Category} {res.CategoryDescription} {res.productName} {Environment.NewLine}");
+                }
+            }
+        }
+	}
+}
---
+++ EPAM.2020.Latuhin.14/North/NorthEntityFramework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NorthEntityFramework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NorthEntityFramework.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5cbf4087-dada-44de-ad6b-64deab0b4466")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
